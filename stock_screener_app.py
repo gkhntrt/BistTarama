f05213184de099cb1912a30f92181996fa881056
gkhntrt <gkhntrt@hotmail.com>
2026-03-29 01:54:36 +0300

    Update stock_screener_app.py

--- stock_screener_app.py
+++ stock_screener_app.py
@@ -111,7 +111,7 @@ def plot_stock_chart(data, ticker_name):
     ax3.grid(True)
 
     # İmza ekleme
-    fig.text(0.5, 0.5, 'Bay-G',
+    fig.text(0.5, 0.5, 'BAY-GT',
              fontsize=50, color='gray', alpha=0.15,
              ha='center', va='center',
              weight='bold', style='italic', rotation=20)
